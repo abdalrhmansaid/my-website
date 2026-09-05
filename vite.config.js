37fd81a49965d7a705380ae8ce0e5047eb590a28
abdalrhmansaid <65103703+abdalrhmansaid@users.noreply.github.com>
2026-09-05 14:04:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
+base: '/my-website/',
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
